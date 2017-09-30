25035c7e64bf1023e5df7da05645cb80a61afce6
Darwin Bautista <daruuin@gmail.com>
2017-09-30 23:25:26 +0800

    Add super fast pure Python implementation of generate_data.py

--- generate_data.py
+++ generate_data.py
@@ -1,17 +1,42 @@
 #!/usr/bin/env python
 
-import subprocess
-import os.path
-import random
 import sys
-import tempfile
+import random
+import glob
+import os.path
+import uuid
+
+from queue import Queue, Empty
+from threading import Thread
 
 import numpy as np
 import cv2
-import pickle
 
-from queue import Queue
-from threading import Thread
+
+def scale_down(img, target_size):
+    src_height, src_width = img.shape
+    src_ratio = src_height/src_width
+    target_width, target_height = target_size
+    if src_ratio < target_height/target_width:
+        dst_size = (int(np.round(target_height/src_ratio)), target_height)
+    else:
+        dst_size = (target_width, int(np.round(target_width*src_ratio)))
+    return cv2.resize(img, dst_size, interpolation=cv2.INTER_AREA)
+
+
+def crop(img, origin, size):
+    width, height = size
+    x, y = origin
+    return img[y:y + height, x:x + width]
+
+
+def center_crop(img, target_size):
+    target_width, target_height = target_size
+    # Note the reverse order of width and height
+    height, width = img.shape
+    x = int(np.round((width - target_width)/2))
+    y = int(np.round((height - target_height)/2))
+    return crop(img, (x, y), target_size)
 
 
 def generate_points():
@@ -22,118 +47,136 @@ def generate_points():
     patch = [
         (x, y),
         (x + 128, y),
-        (x, y + 128),
-        (x + 128, y + 128)
+        (x + 128, y + 128),
+        (x, y + 128)
     ]
     # Perturb
     perturbed_patch = [(x + random.randint(-p, p), y + random.randint(-p, p)) for x, y in patch]
-
-    # We need the inverse homography, so reverse the order
-    return list(zip(perturbed_patch, patch))
+    return np.array(patch), np.array(perturbed_patch)
 
 
-def make_params(points):
-    params = []
-    for o, p in points:
-        # Quick and dirty
-        params.append(str(o).strip('()'))
-        params.append(str(p).strip('()'))
-    return ' '.join(params)
+def warp(img, orig, perturbed, target_size):
+    # Get inverse homography matrix
+    M = cv2.getPerspectiveTransform(np.float32(perturbed), np.float32(orig))
+    return cv2.warpPerspective(img, M, target_size, flags=cv2.INTER_CUBIC)
 
 
-def process_image(image_path, tmp_dir, outdir, num_output=1):
-    img_name = os.path.basename(image_path)
-    tmp_file = os.path.join(tmp_dir, img_name + '.png')
-    # Quick fix to avoid unnecessary work
-    if os.path.isfile(tmp_file):
+def process_image(image_path, num_output=1):
+    # Read as grayscale
+    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    if img.shape < (240, 320):
         return
-    # Resize, center crop, and convert to grayscale
-    subprocess.run(['convert', image_path, '-resize', '320x240^',
-                    '-gravity', 'center', '-extent', '320x240',
-                    '-colorspace', 'gray',
-                    tmp_file])
-    args = [
-        'convert', tmp_file, '-matte',
-        '-virtual-pixel', 'transparent',
-        '-distort', 'Perspective'
-    ]
-    for i in range(num_output):
-        p = generate_points()
-        # Crop params
-        x, y = p[0][0]
-        crop_params = '128x128+' + str(x) + '+' + str(y)
 
-        # Crop orig
-        out_orig = os.path.join(tmp_dir, img_name + '_' + str(i) + '-a.png')
-        subprocess.run(['convert', tmp_file, '-crop', crop_params, out_orig])
+    target_size = (320, 240)
+    img = scale_down(img, target_size)
+    img = center_crop(img, target_size)
 
-        params = make_params(p)
-        out_distorted = os.path.join(tmp_dir, img_name + '_' + str(i) + '-b.png')
-        i_args = args + [params, '-crop', crop_params, out_distorted]
-        print(img_name, params)
-        # Distort and crop
-        subprocess.run(i_args)
-
-        label = []
-        for a, b in p:
-            for x in range(2):
-                label.append(a[x] - b[x])
-        label = np.array(label)
-
-        # Read and normalize images
-        im1 = cv2.imread(out_orig, cv2.IMREAD_GRAYSCALE)
-        im2 = cv2.imread(out_distorted, cv2.IMREAD_GRAYSCALE)
+    patch_size = (128, 128)
+    image_pairs = []
+    offsets = []
+    #orig_points = []
+    #perturbed_points = []
+    while len(offsets) < num_output:
+        orig, perturbed = generate_points()
+        a = crop(img, orig[0], patch_size)
+        b = warp(img, orig, perturbed, target_size)
+        b = crop(b, orig[0], patch_size)
         try:
-            im = np.stack((im1, im2), axis=-1)
+            d = np.stack((a, b), axis=-1)
         except ValueError:
             continue
-
-        data = (im, label)
-        dat = os.path.join(outdir, img_name + '_' + str(i) + '.dat')
-        with open(dat, 'wb') as fdat:
-            pickle.dump(data, fdat)
+        image_pairs.append(d)
+        offset = (perturbed - orig).reshape(-1)
+        offsets.append(offset)
+        #orig_points.append(orig)
+        #perturbed_points.append(perturbed)
+    print('done:', image_path)
+    return image_pairs, offsets
 
 
 class Worker(Thread):
 
-   def __init__(self, queue):
+   def __init__(self, input_queue, output_queue, num_samples):
        Thread.__init__(self)
-       self.queue = queue
+       self.input_queue = input_queue
+       self.output_queue = output_queue
+       self.num_samples = num_samples
 
    def run(self):
        while True:
-           # Get the work from the queue and expand the tuple
-           img_path, output_dir, samples = self.queue.get()
-           process_image(img_path, output_dir, samples)
-           self.queue.task_done()
+           img_path = self.input_queue.get()
+           if img_path is None:
+               break
+           output = process_image(img_path, self.num_samples)
+           self.input_queue.task_done()
+           if output is not None:
+               self.output_queue.put(output)
+
+
+def pack(outdir, image_pairs, offsets):
+    name = str(uuid.uuid4())
+    pack = os.path.join(outdir, name + '.npz')
+    with open(pack, 'wb') as f:
+        np.savez(f, images=np.stack(image_pairs), offsets=np.stack(offsets))
+    print('bundled:', name)
+
+
+def bundle(queue, outdir):
+    image_pairs = []
+    offsets = []
+    #orig_points = []
+    #perturbed_points = []
+    while True:
+        try:
+            d, o = queue.get(timeout=10)
+        except Empty:
+            break
+        image_pairs.extend(d)
+        offsets.extend(o)
+        #orig_points.extend(orig)
+        #perturbed_points.extend(perturbed)
+
+        if len(image_pairs) >= 7680:
+            pack(outdir, image_pairs, offsets)
+            image_pairs = []
+            offsets = []
+        queue.task_done()
+
+    if image_pairs:
+        pack(outdir, image_pairs, offsets)
 
 
 def main():
-    if len(sys.argv) != 4:
-        print('Usage: generate_data.py <input dir> <output dir> <samples per input>')
+    if len(sys.argv) < 4:
+        print('Usage: generate_data.py <output dir> <samples per input> <INPUT DIRS...>')
         exit(1)
-    input_dir = sys.argv[1]
-    output_dir = sys.argv[2]
-    samples = int(sys.argv[3])
+    output_dir = sys.argv[1]
+    samples = int(sys.argv[2])
+    input_dirs = sys.argv[3:]
 
-    tmp_dir = tempfile.TemporaryDirectory().name
-    
     # Create a queue to communicate with the worker threads
-    queue = Queue()
-    # Create 4 worker threads
-    for x in range(4):
-        worker = Worker(queue)
-        # Setting daemon to True will let the main thread exit even though the workers are blocking
-        worker.daemon = True
+    input_queue = Queue()
+    output_queue = Queue()
+
+    num_workers = 8
+    workers = []
+    # Create worker threads
+    for i in range(num_workers):
+        worker = Worker(input_queue, output_queue, samples)
         worker.start()
-    # Put the tasks into the queue as a tuple 
-    for i in next(os.walk(input_dir))[-1]:
-        img_path = os.path.join(input_dir, i)
-        queue.put((img_path, tmp_dir, output_dir, samples))
-        
-    # Causes the main thread to wait for the queue to finish processing all the tasks
-    queue.join()
-   
+        workers.append(worker)
+
+    for d in input_dirs:
+        for i in glob.iglob(os.path.join(d, '*.jpg')):
+            input_queue.put(i)
+
+    bundle(output_queue, output_dir)
+
+    input_queue.join()
+    for i in range(num_workers):
+        input_queue.put(None)
+    for worker in workers:
+        worker.join()
 
 
 if __name__ == '__main__':
